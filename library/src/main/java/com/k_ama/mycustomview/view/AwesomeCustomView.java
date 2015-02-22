c4839a93f03639d6b5a8ca4a5702fee509b4bf59
keigo_amai <keigo.amai@glpgs.com>
2015-02-23 01:49:07 +0900

    カスタムビューの作成

---
+++ library/src/main/java/com/k_ama/mycustomview/view/AwesomeCustomView.java
@@ -0,0 +1,16 @@
+package com.k_ama.mycustomview.view;
+
+import android.content.Context;
+import android.widget.TextView;
+
+/**
+ * "Awesome"
+ *
+ * Created by keigo.amai / Galapagos.Inc on 2015/02/23.
+ */
+public class AwesomeCustomView extends TextView {
+    public AwesomeCustomView(Context context) {
+        super(context);
+        setText("AwesomeTextView");
+    }
+}
